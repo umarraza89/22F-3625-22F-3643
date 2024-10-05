30f388eedd7e1623ae0ad49d178d81cc57626806
Ahmad Ijaz <mahmadijaz192@gmail.com>
2024-10-05 18:17:26 +0500

    added removebook function to library class

---
+++ src/githubsubmissionn/Library.java
@@ -0,0 +1,28 @@
+package githubsubmissionn;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<String> books = new ArrayList<>();
+
+    public void addBook(String bookName) {
+        books.add(bookName);
+        System.out.println(bookName + " added to library.");
+    }
+
+    
+    public boolean searchBook(String bookName) {
+        return books.contains(bookName);
+    }
+    
+    public void removeBook(String bookName) {
+        if (books.remove(bookName)) {
+            System.out.println(bookName + " removed from the library.");
+        } else {
+            System.out.println(bookName + " not found in the library.");
+        }
+    }
+
+    public void listBooks() {
+        System.out.println("Books in library: " + books);
+    }   }
---
+++ src/githubsubmissionn/Main.java
@@ -0,0 +1,11 @@
+package githubsubmissionn;
+
+public class Main {
+    public static void main(String[] args) {
+        Library myLibrary = new Library();
+        myLibrary.addBook("Harry Potter");
+        myLibrary.addBook("Lord of the Rings");
+        myLibrary.listBooks();
+    }
+}
+
